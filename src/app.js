a78cf2909a6cf3b09f10e91458da73b0cb220a37
Manishat10 <140322516+Manishat10@users.noreply.github.com>
2025-09-12 12:20:10 +0530

    app.js modified

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
-const taskRoutes = require('./routes/taskRoutes');  // 👈 add this
-
+const taskRoutes = require('./routes/taskRoutes'); 
+const commentRoutes=require('./routes/commentRoutes');
 const app = express();
 app.use(express.json());
-
 // Routes
 app.use('/api/users', userRoutes);
-app.use('/api/tasks', taskRoutes);   // 👈 add this line
-
+app.use('/api/tasks', taskRoutes); 
+app.use('/api/tasks/:id/comments', commentRoutes);
 module.exports = app;
