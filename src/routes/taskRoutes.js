0d402c1e82c501e38254315ccf84dba1f0917cf9
Manishat10 <140322516+Manishat10@users.noreply.github.com>
2025-09-12 12:21:06 +0530

    task routes added routes related to tasks

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,9 +1,11 @@
 const express =require("express");
-const {createTask,getTask,updateTask,deleteTask}=require("../controllers/taskController");
+const {createTask,getTask,updateTask,deleteTask,getFilteredTasks}=require("../controllers/taskController");
 const auth =require("../middlewares/auth");
+const {checkTaskViewAccess,checkTaskOwnerAccess}=require("../middlewares/taskAccess")
 const router =express.Router();
 router.post("/",auth,createTask);
-router.get("/:id",auth,getTask);
-router.put("/:id",auth,updateTask);
-router.delete("/:id",auth,deleteTask);
+router.get("/:id",auth,checkTaskViewAccess,getTask);
+router.put("/:id",auth,checkTaskOwnerAccess,updateTask);
+router.delete("/:id",auth,checkTaskOwnerAccess,deleteTask);
+router.get("/",auth,getFilteredTasks);
 module.exports=router;
