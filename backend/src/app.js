c01efe09f1d957b90cfaa05774d17d026b98a6fd
Manishat10 <140322516+Manishat10@users.noreply.github.com>
2025-09-16 18:42:53 +0530

    remaining files

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,11 @@ const express = require('express');
 const userRoutes = require('./routes/userRoutes');
 const taskRoutes = require('./routes/taskRoutes'); 
 const commentRoutes=require('./routes/commentRoutes');
+const cors = require("cors");
 const app = express();
+app.use(cors({
+  origin: "http://localhost:5173" // or "*" for all origins (not recommended for production)
+}));
 app.use(express.json());
 // Routes
 app.use('/api/users', userRoutes);
